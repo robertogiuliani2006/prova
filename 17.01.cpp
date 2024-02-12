8812a4065f6904ec366ee748bcb753a53a87eaea
robertogiuliani2006 <122273844+robertogiuliani2006@users.noreply.github.com>
2024-02-12 09:52:36 +0100

    Add files via upload

---
+++ 17.01.cpp
@@ -0,0 +1,27 @@
+//ROBERTO GIULIANI 4AI
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+float CalcoloArea(float raggio);
+
+int main() {
+    using namespace std;
+
+    float raggio;
+
+    cout << "Inserisci il raggio del cerchio: ";
+    cin >> raggio;
+
+    //calocolo + stampa
+    float area = CalcoloArea(raggio);
+    cout << "L'area del cerchio con raggio " << raggio << " e': " << area << endl;
+
+    return 0;
+}
+
+float CalcoloArea(float raggio) {
+    const float pi = 3.1415;
+    return pi * raggio * raggio;
+}
